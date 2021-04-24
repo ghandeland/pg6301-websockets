4302e7dfb8e1bfb024facbbd62843fc84b7bd9c7
ghandeland <gabriel.handeland@gmail.com>
2021-04-25 00:01:07 +0200

    Test message sending funcitionality with simulate

--- __tests__/client/ChatView.test.jsx
+++ __tests__/client/ChatView.test.jsx
@@ -1,6 +1,7 @@
 import { ChatView } from "../../src/client/ChatPage";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Simulate } from 'react-dom/test-utils'
 
 describe("chat view", () => {
     it("shows chat log as messages", () => {
@@ -14,11 +15,30 @@ describe("chat view", () => {
           container
         );
                 
-        expect(
-          container.querySelector('.chat-msg')
-            .textContent
-        ).toEqual('hello');
-        expect(container).toMatchSnapshot();
-    })
+    expect(container.querySelector('.chat-msg').textContent).toEqual('hello');
+    expect(container).toMatchSnapshot();
+    });
     
+    it("can send a chat message", () => {
+        const onSend = jest.fn();
+        
+        const container = document.createElement("div");
+        ReactDOM.render(
+          <ChatView chatLog={[]} onSendMessage={onSend} />,
+          container
+        );
+        
+        Simulate.change(container.querySelector("form input"), {
+           target: { value: "Hello foo" }
+        });
+        
+        expect(
+          container.querySelector('form input').getAttribute('value')
+        ).toEqual('Hello foo');
+        
+        Simulate.submit(container.querySelector("form"));
+        expect(onSend).toBeCalledWith('Hello foo');
+        expect(container.querySelector("form input").getAttribute("value")).toEqual("");
+        
+    });
 });
